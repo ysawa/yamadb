8dbbebe187153a9335c71f859e24f04ffeef2485
Y Sawa <yoshihirosawa@gmail.com>
2011-07-16 19:05:34 +0900

    layout top page

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,6 @@
 class MainController < ApplicationController
   def index
-    @peaks = Peak.all
+    @peaks = Peak.desc(:updated_at).limit(3)
+    @equipments = Equipment.desc(:updated_at).limit(3)
   end
 end
