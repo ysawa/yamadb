d886ff8460eec75d2c09708c4b1afad43ea960b3
Y Sawa <yoshihirosawa@gmail.com>
2011-07-20 17:22:10 +0900

    modify email address to send emails

--- config/initializers/application.rb
+++ config/initializers/application.rb
@@ -5,7 +5,7 @@ module Yamadb
     @@mailer = ::Yamadb::SYSTEM['mailer']
 
     # define functions to get mailer configurations
-    function_names = %w(default_url_options delivery_method sendmail_settings smtp_settings)
+    function_names = %w(default_email default_url_options delivery_method sendmail_settings smtp_settings)
     function_names.each do |function_name|
       module_eval(<<-EOS)
         def #{function_name}
