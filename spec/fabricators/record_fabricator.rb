13d328ed2bc5bbf2d701bca449342d8f249d37c3
Y Sawa <yoshihirosawa@gmail.com>
2011-08-01 22:33:26 +0900

    modify record tests

--- spec/fabricators/record_fabricator.rb
+++ spec/fabricators/record_fabricator.rb
@@ -1,6 +1,6 @@
 Fabricator(:record) do
-  name "MyString"
-  content "MyString"
-  start "2011-07-30 14:55:53"
-  finish "2011-07-30 14:55:53"
+  name '夏山山行 川苔山'
+  content '09:06 奥多摩駅到着\n10:04 バス出発'
+  start '2011-07-30 08:55:00'
+  finish '2011-07-30 16:55:00'
 end
--- spec/views/records/show.html.haml_spec.rb
+++ spec/views/records/show.html.haml_spec.rb
@@ -3,16 +3,12 @@ require 'spec_helper'
 describe "records/show.html.haml" do
   before(:each) do
     @record = assign(:record, stub_model(Record,
-      :name => "Name",
-      :content => "Content"
+      Fabricate.build(:record).attributes
     ))
   end
 
   it "renders attributes in <p>" do
     render
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    rendered.should match(/Name/)
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    rendered.should match(/Content/)
+    rendered.should match(@record.name)
   end
 end
