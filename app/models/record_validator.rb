c0c2a579dc6bea4e5a1cabce8a0b99bf220f9829
Y Sawa <yoshihirosawa@gmail.com>
2011-08-02 20:39:49 +0900

    modify record form and extract partial of record

--- app/models/record_validator.rb
+++ app/models/record_validator.rb
@@ -3,5 +3,8 @@ class RecordValidator < ActiveModel::Validator
     %w(name content start finish).each do |attr_name|
       record.validates_presence_of attr_name
     end
+    if record.start && record.finish
+      record.errors.add(:finish, :invalid) if record.start > record.finish
+    end
   end
 end
