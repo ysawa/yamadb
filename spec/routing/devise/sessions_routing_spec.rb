8a991909df7059fa69d333d78be67ab3c823f00c
Y Sawa <yoshihirosawa@gmail.com>
2011-08-18 22:34:46 +0900

    modify devise#sessions tests to pass

--- spec/routing/devise/sessions_routing_spec.rb
+++ spec/routing/devise/sessions_routing_spec.rb
@@ -6,7 +6,6 @@ describe Devise::SessionsController do
   end
   describe "routing" do
     it "routes to #new" do
-      get(new_user_session_url(:host => 'test', :protocol => 'http')).should_not be_routable
       get(new_user_session_url(:host => 'test', :protocol => 'https')).should route_to("devise/sessions#new")
     end
   end
