fb50fd304229b832b98188d1260090f014198067
Y Sawa <yoshihirosawa@gmail.com>
2011-08-19 17:34:55 +0900

    modify many links not to connect https

--- app/helpers/google_map_helper.rb
+++ app/helpers/google_map_helper.rb
@@ -2,4 +2,14 @@ module GoogleMapHelper
   def google_map_include_tag
     javascript_include_tag "http://maps.google.com/maps/api/js?sensor=false&language=ja"
   end
+
+  def google_map_peak_image(location, options = {})
+    options.stringify_keys!
+    if location.present? && location.size == 2
+      protocol = (options['secure'] ? 'https' : 'http')
+      image_tag "#{protocol}://maps.google.com/maps/api/staticmap?center=#{location[0]},#{location[1]}&zoom=10&size=160x160&sensor=false"
+    else
+      image_tag '/images/common/no_picture.png'
+    end
+  end
 end
